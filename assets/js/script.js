bd6f7800b78fe86006b981fa06081d24563d0843
Mohammadsaed12 <mohammadsaedtoto12345@gmail.com>
2025-05-16 15:37:05 -0700

    enhance preveios code

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,11 +16,14 @@ let currentPlayer = player1;
 let gameOver = false;
 let board = document.querySelectorAll('.cell');
 //add event listener to each cell
-board.forEach((cell) => {
+
+board.forEach((cell,index) => {
+    cell.setAttribute('name', index);
+      
 cell.addEventListener('click', () => {
     if (cell.innerHTML === "" && !gameOver) {
         cell.innerHTML = currentPlayer.value;
-        currentPlayer.squers.push(cell);
+        currentPlayer.squers.push(cell.getAttribute('name'));
        
         if (currentPlayer === player1) {
             cell.classList.add('cellX');
@@ -48,5 +51,6 @@ resetButton.addEventListener('click', () => {
     currentPlayer = player1;
     gameOver = false;
 });
-
+console.log(player1);
+console.log(player2);
 
