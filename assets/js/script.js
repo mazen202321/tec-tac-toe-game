e76089ad8d0dbe552c8d7f368debb099911a27ba
Mohammadsaed12 <mohammadsaedtoto12345@gmail.com>
2025-05-19 22:45:35 -0700

    add check winner function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 
-const possiboleWines=[[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]];
+const possiboleWines=[["0","1","2"],["3","4","5"],["6","7","8"],["0","4","8"],["2","4","6"],["0","3","6"],["1","4","7"],["2","5","8"]];
+
 let player1 = {
     name: "Player 1",
     value: "X",
@@ -27,17 +28,22 @@ cell.addEventListener('click', () => {
        
         if (currentPlayer === player1) {
             cell.classList.add('cellX');
-            currentPlayer = player2;
-
+           
         }else {
             cell.classList.add('cellO');
-            currentPlayer = player1;
+           
         }
-
+        checkWinner(currentPlayer);
+        currentPlayer==player1?currentPlayer=player2:currentPlayer=player1;
        
     }
     });
 });
+ document.querySelector('#plyer1').innerHTML= player1.name;
+ document.querySelector('#player1_result').innerHTML= player1.score;
+ document.querySelector('#player2').innerHTML= player2.name;
+ document.querySelector('#player2_result').innerHTML= player2.score;
+
 //add event listener to reset button
 let resetButton = document.querySelector('#reset');
 resetButton.addEventListener('click', () => {
@@ -54,3 +60,33 @@ resetButton.addEventListener('click', () => {
 console.log(player1);
 console.log(player2);
 
+function checkWinner(currentPlayer) {
+    console.log("checking winner of "+currentPlayer.name);
+  
+    if (currentPlayer.squers.length >= 3) {
+        possiboleWines.forEach((wine) => {
+            console.log("Pwins = "+wine);
+            let squers = currentPlayer.squers;
+            console.log("squers = "+squers);
+            if (currentPlayer.squers.includes(wine[0]) && currentPlayer.squers.includes(wine[1]) && currentPlayer.squers.includes(wine[2])) {
+                console.log("true");
+                console.log("winner");
+                gameOver = true;
+                alert(currentPlayer.name + " wins!");
+                currentPlayer.score++;
+                
+                board.forEach((cell) => {
+                    cell.innerHTML = "";
+                    cell.classList.remove('cellX');
+                    cell.classList.remove('cellO');
+                });
+                player1.squers = [];
+                player2.squers = [];
+                currentPlayer = player1;
+            }
+           
+        });
+     
+   
+    }
+}
